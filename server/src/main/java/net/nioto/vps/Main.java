99312ef358f5ad69295d379dab14f549e1b68fe8
nioto <nioto.org@gmail.com>
2015-03-01 18:51:01 +0100

    Initial commit for server part
    Token Generation on java server

---
+++ server/src/main/java/net/nioto/vps/Main.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package net.nioto.vps;
+
+import spark.servlet.SparkApplication;
+
+/**
+ * @author tonio
+ *
+ */
+public class Main implements SparkApplication {
+
+	/**
+	 * 
+	 */
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see spark.servlet.SparkApplication#init()
+	 */
+	public void init() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ server/src/main/java/net/nioto/vps/MyFilter.java
@@ -0,0 +1,11 @@
+package net.nioto.vps;
+
+import spark.servlet.SparkFilter;
+
+public class MyFilter extends SparkFilter {
+
+	public MyFilter() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
